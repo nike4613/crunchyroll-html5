19581f0b689342a47a2eb3f5caa4cc4ab51ebd9f
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-05-21 22:00:01 -0500

    Added more complex and complete styling for options popup

--- src/app/player/options/OptionsPopup.tsx
+++ src/app/player/options/OptionsPopup.tsx
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 import { IStorageSymbol, IStorage } from '../../storage/IStorage';
 import Portal from '../../libs/Portal';
 import { WebExtensionMechanism } from '../../storage/mechanism/WebExtensionMechanism';
+import { updateQualitySettings } from '../StandardPlayer';
 
 export interface IOptionsPopupProps {
   isWebextension: boolean;
@@ -13,6 +14,8 @@ export interface IOptionsPopupState {
 
 export class OptionsPopup extends Component<IOptionsPopupProps, IOptionsPopupState> {
 
+  private _syncCheckbox: HTMLInputElement;
+
   open() {
     this.setState({visible: true});
   }
@@ -21,23 +24,35 @@ export class OptionsPopup extends Component<IOptionsPopupProps, IOptionsPopupSta
     const open = () => this.setState({visible: true});
     const close = () => this.setState({visible: false});
 
-    let inBrowser = props.isWebextension;
+    let webext = props.isWebextension;
     
     let syncing = false;
-    if (inBrowser) {
+    if (webext) {
       syncing = WebExtensionMechanism.sync;
     }
+
+    const synccheck = (cb: HTMLInputElement) => this._syncCheckbox = cb;
+
+    const save = () => {
+      if (webext) {
+        WebExtensionMechanism.sync = this._syncCheckbox.checked;
+        updateQualitySettings(); // this saves quality to storage; that's whats important
+      }
+    };
 
     // Portal injects the children into the specified element
     return (
       this.state.visible ? (
-        <Portal into="div#showmedia_video">
-          <div class="options popup" onClick={close}>
-            { inBrowser ? (
-              [<span className="opt-name">Use browser.storage.sync</span>,  <input type="checkbox" checked={syncing}></input>]
-            ): null }
-            <span className="opt-name">NONOPT</span>  <input type="checkbox" checked={false}></input>
-            <button>BUTN</button>
+        <Portal into="body">
+          <div class="popup-container">
+            <div class="options popup">
+              <span className="close-button" onClick={close}></span>
+
+              { webext ? (
+                [<span className="opt-name">Use <code>browser.storage.sync</code></span>,  <input type="checkbox" checked={syncing} ref={synccheck}></input>]
+              ): null }
+              <div><button onClick={save}>Save</button><span className="spacer"></span><button onClick={close}>Cancel</button></div>
+            </div>
           </div>
         </Portal>
       ) : null
