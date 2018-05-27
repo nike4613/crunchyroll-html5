326499a6a7597a78c19c3f0593e12e9a9de97c7a
Anairkoen Schno <aaron.cirr.com@gmail.com>
2018-05-26 21:23:30 -0500

    Fixed indention in `src/app/libs/Portal.tsx` and `src/styles/player/options.scss`

--- src/app/libs/Portal.tsx
+++ src/app/libs/Portal.tsx
@@ -17,53 +17,53 @@ export default class Portal extends Component<IPortalProperties,{}> {
   private intoPointer: string|Element;
   private into: Element;
 
-	componentDidUpdate(props: IPortalProperties) {
-		for (let i in props) {
-			if (props[i]!==this.props[i]) {
-				return setTimeout(this.renderLayer);
-			}
+  componentDidUpdate(props: IPortalProperties) {
+    for (let i in props) {
+      if (props[i]!==this.props[i]) {
+        return setTimeout(this.renderLayer);
+      }
     }
     return;
-	}
+  }
 
-	componentDidMount() {
-		this.isMounted=true;
-		this.renderLayer = this.renderLayer.bind(this);
-		this.renderLayer();
-	}
+  componentDidMount() {
+    this.isMounted=true;
+    this.renderLayer = this.renderLayer.bind(this);
+    this.renderLayer();
+  }
 
-	componentWillUnmount() {
-		this.renderLayer(false);
-		this.isMounted=false;
-		if (this.remote) this.remote.parentNode!.removeChild(this.remote);
-	}
+  componentWillUnmount() {
+    this.renderLayer(false);
+    this.isMounted=false;
+    if (this.remote) this.remote.parentNode!.removeChild(this.remote);
+  }
 
-	findNode(node: string|Element): Element {
-		return typeof node==='string' ? document.querySelector(node)! : node;
-	}
+  findNode(node: string|Element): Element {
+    return typeof node==='string' ? document.querySelector(node)! : node;
+  }
 
-	renderLayer(show=true) {
-		if (!this.isMounted) return;
+  renderLayer(show=true) {
+    if (!this.isMounted) return;
 
-		// clean up old node if moving bases:
-		if (this.props.into!==this.intoPointer) {
-			this.intoPointer = this.props.into;
-			if (this.into && this.remote) {
-				this.remote = render(<PortalProxy />, this.into, this.remote);
-			}
-			this.into = this.findNode(this.props.into);
-		}
+    // clean up old node if moving bases:
+    if (this.props.into!==this.intoPointer) {
+      this.intoPointer = this.props.into;
+      if (this.into && this.remote) {
+        this.remote = render(<PortalProxy />, this.into, this.remote);
+      }
+      this.into = this.findNode(this.props.into);
+    }
 
-		this.remote = render((
-			<PortalProxy context={this.context}>
-				{ show && this.props.children || null }
-			</PortalProxy>
-		), this.into, this.remote);
-	}
+    this.remote = render((
+      <PortalProxy context={this.context}>
+        { show && this.props.children || null }
+      </PortalProxy>
+    ), this.into, this.remote);
+  }
 
-	render() {
-		return null;
-	}
+  render() {
+    return null;
+  }
 }
 
 interface IPortalProxyProps {
@@ -72,10 +72,10 @@ interface IPortalProxyProps {
 // high-order component that renders its first child if it exists.
 // used as a conditional rendering proxy.
 class PortalProxy extends Component<IPortalProxyProps,{}> {
-	getChildContext() {
-		return this.props.context;
-	}
-	render(props: IPortalProxyProps & ComponentProps<this>): JSX.Element | null {
-		return props.children && props.children[0] || null;
-	}
+  getChildContext() {
+    return this.props.context;
+  }
+  render(props: IPortalProxyProps & ComponentProps<this>): JSX.Element | null {
+    return props.children && props.children[0] || null;
+  }
 }
